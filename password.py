d2c11ac62c40a1afdc1fd0a99b7c160c4e39ecd6
Kiru-axis <peter.kiru@student.moringaschool.com>
2021-05-31 12:47:38 +0300

    adding the save method to the main file

--- password.py
+++ password.py
@@ -4,11 +4,39 @@ class Cridential:
     Class that generates new instances of passwords
     """
     password_list = []
-
+# 1. Initialisation
     def __init__(self,account,username,password):
         self.account = account
         self.username = username
         self.password = password
+
+        
+#2. save acount password / save multiple account passwords
+    def save_password(self):
+        '''
+        save_password method saves password objects into password_list
+        '''
+        Cridential.password_list.append(self)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 # User class 
@@ -20,3 +48,5 @@ class User:
     def __init__(self,user,login_password):
         self.user = user
         self.login_password = login_password
+
+

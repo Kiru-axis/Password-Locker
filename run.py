8f33ed30c02363a47c9247b2ecd4f07cf43b4e18
Kiru-axis <peter.kiru@student.moringaschool.com>
2021-05-31 13:24:54 +0300

    all the mini functions required by main()

--- run.py
+++ run.py
@@ -4,3 +4,59 @@
 from password import Cridential,User
 import random
 import string
+
+# User-----------------------------------------
+# 1. Creating created_user_password details
+def save_user(login):
+    """
+    Function to save created_username created_user_password details
+    """
+    login.save_password()
+
+
+# Cridentials----------------------------------------------------
+# 1. Creating an account 
+def create_password(account,username,password):
+    '''
+    Function to create a new Account
+    '''
+    new_password = Cridential(account,username,password)
+    return new_password
+
+# 1. Save the account
+def save_password(account):
+    '''
+    Function to save Account
+    '''
+    account.save_password()
+
+
+# 3. Delete account
+def del_password(account):
+    '''
+    Function to delete an account
+    '''
+    account.delete_password()
+
+# 4. Finding account
+def find_account(account):
+    '''
+    Function that finds password by account and returns the account
+    '''
+    return Cridential.find_by_account(account)
+
+
+# Check if an account exists
+def check_existing_password(account):
+    """
+    Function that returs all the accounts
+    """
+    return Cridential.password_exists(account)
+
+# Display all password and account
+def display_password():
+    """
+    Function returns all accounts and their passwords
+    """
+    return Cridential.display_all_passwords()
+

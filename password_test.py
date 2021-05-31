acd649756ef65f6a8bae12cbc138536f319ae045
Kiru-axis <peter.kiru@student.moringaschool.com>
2021-05-31 12:37:54 +0300

    importing all the relevant modules to test

---
+++ password_test.py
@@ -0,0 +1,12 @@
+import unittest
+from password import Cridential
+from password import User
+
+# Unittest for cridental
+class TestPassword(unittest.TestCase):
+    '''
+    Test class that defines test cases for the paswords class behaviours.
+
+    Args:
+        unittest.TestCase: TestCase class that helps in creating test cases
+    '''

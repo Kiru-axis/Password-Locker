e73edb48af987e753ac5acdd236aac317cf1ce21
Kiru-axis <peter.kiru@student.moringaschool.com>
2021-05-31 12:36:51 +0300

    Initialising the user class

--- password.py
+++ password.py
@@ -9,3 +9,14 @@ class Cridential:
         self.account = account
         self.username = username
         self.password = password
+
+
+# User class 
+class User:
+    user_list = []
+    """
+    generates new instances of users to login
+    """
+    def __init__(self,user,login_password):
+        self.user = user
+        self.login_password = login_password
